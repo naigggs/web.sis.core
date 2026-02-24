9f62b6a2119e209d20f2ea3de5b6ae7007b9df94
Gian Cabigting <gdcabigting.dev@gmail.com>
2026-02-25 02:53:32 +0800

    refactor: improve formatting and indentation in auth middleware

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -6,36 +6,40 @@ import { env } from "../../config/env"
 import { createResponse } from "../utils/response/response"
 
 export const authMiddleware = async (c: Context, next: Next) => {
-    try {
-        const accessToken = getCookie(c, "accessToken")
+  try {
+    const accessToken = getCookie(c, "accessToken")
 
-        if (!accessToken) {
-            const response = createResponse(
-                false,
-                "Unauthorized. Access denied.",
-                null,
-                ["Token is required"],
-                null,
-                c.req.header("x-request-id"),
-            )
-            return c.json(response, 401)
-        }
-
-        const payload = await verify(accessToken, env.JWT_ACCESS_SECRET_KEY!)
-        c.set("user", payload)
-
-        await next()
-    } catch (error) {
-        const response = createResponse(
-            false,
-            "Unauthorized. Access denied.",
-            null,
-            ["Token is invalid or expired"],
-            null,
-            c.req.header("x-request-id"),
-        )
-        return c.json(response, 401)
+    if (!accessToken) {
+      const response = createResponse(
+        false,
+        "Unauthorized. Access denied.",
+        null,
+        ["Token is required"],
+        null,
+        c.req.header("x-request-id"),
+      )
+      return c.json(response, 401)
     }
+
+    const payload = await verify(
+      accessToken,
+      env.JWT_ACCESS_SECRET_KEY!,
+      "HS256",
+    )
+    c.set("user", payload)
+
+    await next()
+  } catch (error) {
+    const response = createResponse(
+      false,
+      "Unauthorized. Access denied.",
+      null,
+      ["Token is invalid or expired"],
+      null,
+      c.req.header("x-request-id"),
+    )
+    return c.json(response, 401)
+  }
 }
 
 export const requireAuth = authMiddleware
