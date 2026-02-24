44ec4ae882c5bebffb657e9e778ab2e33b6ae67c
Gian Cabigting <gdcabigting.dev@gmail.com>
2026-02-25 03:04:50 +0800

    feat: add student routes to v1 router

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono"
 import { authRoutes } from "../../domain/auth/auth.routes"
+import { studentRoutes } from "../../domain/student/student.routes"
 
 const v1Router = new Hono()
 
 v1Router.route("/auth", authRoutes)
+v1Router.route("/students", studentRoutes)
 
 export default v1Router
