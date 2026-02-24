bbd345b6f795ab912b2300cbbd822b4d53d38391
Gian Cabigting <gdcabigting.dev@gmail.com>
2026-02-25 03:29:36 +0800

    feat: add routes for subjects and grades in v1 API

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -2,11 +2,15 @@ import { Hono } from "hono"
 import { authRoutes } from "../../domain/auth/auth.routes"
 import { studentRoutes } from "../../domain/student/student.routes"
 import { courseRoutes } from "../../domain/course/course.routes"
+import { subjectRoutes } from "../../domain/subject/subject.routes"
+import { gradeRoutes } from "../../domain/grade/grade.routes"
 
 const v1Router = new Hono()
 
 v1Router.route("/auth", authRoutes)
 v1Router.route("/students", studentRoutes)
 v1Router.route("/courses", courseRoutes)
+v1Router.route("/subjects", subjectRoutes)
+v1Router.route("/grades", gradeRoutes)
 
 export default v1Router
